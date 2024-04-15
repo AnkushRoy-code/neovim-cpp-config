17f877fd112a6fab865b5d165b3dca1cd153c36e
Ankush Roy <128731129+AnkushRoy-code@users.noreply.github.com>
2024-04-15 13:01:42 +0530

    Add files via upload

---
+++ nvim/lua/plugins/neo-tree.lua
@@ -0,0 +1,35 @@
+return {
+	{
+		"nvim-neo-tree/neo-tree.nvim",
+		branch = "v3.x",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-tree/nvim-web-devicons",
+			"MunifTanjim/nui.nvim",
+		},
+
+		keys = {
+			{
+				"<leader>fe",
+				function()
+					require("neo-tree.command").execute({ toggle = true, dir = vim.uv.cwd() })
+				end,
+				desc = "Explorer NeoTree (cwd)",
+			},
+			{
+				"<leader>ge",
+				function()
+					require("neo-tree.command").execute({ source = "git_status", toggle = true })
+				end,
+				desc = "Git Explorer",
+			},
+			{
+				"<leader>be",
+				function()
+					require("neo-tree.command").execute({ source = "buffers", toggle = true })
+				end,
+				desc = "Buffer Explorer",
+			},
+		},
+	},
+}
